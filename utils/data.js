35481c2305e4bede78ba4821a430eff6fc422482
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-04-07 11:34:42 +0100

    Create data.js

---
+++ utils/data.js
@@ -0,0 +1,166 @@
+const data = {
+    products: [
+        {
+            name: 'Bakugo',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Hero Academia",
+            price: 60,
+            image: "/assets/Bakugo.jpg",
+            featured: true
+        },
+        {
+            name: 'Ban',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Seven Deadly Sins",
+            price: 50,
+            image: "/assets/Ban.jpg",
+            featured: true
+        },
+        {
+            name: 'Deku',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Hero Academia",
+            price: 70,
+            image: "/assets/Deku.jpg",
+            featured: true
+        },
+        {
+            name: 'Elizabeth',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Seven Deadly Sins",
+            price: 50,
+            image: "/assets/Elizabeth.jpg",
+            featured: true
+        },
+        {
+            name: 'Hinata',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 60,
+            image: "/assets/Hinata.jpg",
+            featured: true
+        },
+        {
+            name: 'Ichigo',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Bleach",
+            price: 70,
+            image: "/assets/Ichigo-Kurosaki.jpg",
+            featured: true
+        },
+        {
+            name: 'Itachi',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 60,
+            image: "/assets/Itachi-Uchiha.jpg",
+            featured: true
+        },
+        {
+            name: 'Kakashi',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 60,
+            image: "/assets/Kakashi.jpg",
+            featured: true
+        },
+        {
+            name: 'Luffy',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "One Piece",
+            price: 70,
+            image: "/assets/Luffy.jpg",
+            featured: true
+        },
+        {
+            name: 'Meliodas',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Seven Deadly Sins",
+            price: 70,
+            image: "/assets/Meliodas.jpg",
+            featured: true
+        },
+        {
+            name: 'Minato',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 50,
+            image: "/assets/Minato.jpg",
+            featured: true
+        },
+        {
+            name: 'Naruto',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 70,
+            image: "/assets/Naruto.jpg",
+            featured: true
+        },
+        {
+            name: 'Nezuko',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Demon Slayer",
+            price: 60,
+            image: "/assets/Nezuko-Kamado.jpg",
+            featured: true
+        },
+        {
+            name: 'Ochaco',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Hero Academia",
+            price: 60,
+            image: "/assets/Ochaco-Uraraka.jpg",
+            featured: true
+        },
+        {
+            name: 'Zoro',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "One Piece",
+            price: 60,
+            image: "/assets/Roronoa-Zoro.jpg",
+            featured: true
+        },
+        {
+            name: 'Sakura',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 60,
+            image: "/assets/Sakura.jpg",
+            featured: true
+        },
+        {
+            name: 'Sasuke',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Naruto",
+            price: 60,
+            image: "/assets/Sasuke-Uchiha.jpg",
+            featured: true
+        },
+        {
+            name: 'Gojo',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Jujutsu Kaisen",
+            price: 70,
+            image: "/assets/Satoru-Gojo.jpg",
+            featured: true
+        },
+        {
+            name: 'Tanjiro',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Demon Slayer",
+            price: 70,
+            image: "/assets/Tanjiro-Kamado.jpg",
+            featured: true
+        },
+        {
+            name: 'Todoroki',
+            desc: 'lorem ipsum lorem ippppppppppppsum',
+            category: "Hero Academia",
+            price: 60,
+            image: "/assets/Todoroki.jpg",
+            featured: true
+        },
+    ],
+}
+
+export default data
